895a5d3795b2ee2f9b41311a097a5ce0247d801b
Sterling <sterling.kovash@gmail.com>
2022-01-21 09:41:24 -0600

    MadLibs game

---
+++ MadLibs.py
@@ -0,0 +1,15 @@
+print("We are about to play a game of MadLibs")
+P_noun = input("Enter a proper noun: ")
+verb = input("Enter a verb ending in -ing: ")
+adverb = input("Enter a adverb: ")
+place1 = input("Enter a place: ")
+S_noun = input("Enter a singular common noun: ")
+adjective = input("Enter an adjective: ")
+verb2 = input("Enter a past participle verb: ")
+adverb2 = input("Enter another adverb: ")
+adjective2 = input("Enter another adjective: ")
+S_noun2 = input("Enter another singular common noun: ")
+adverb3 = input("Enter another adverb: ")
+
+print(f"One day, {P_noun} was {adverb} {verb} from the {place1}, when they saw a {adjective} {S_noun}! Instead of confronting it, they decided to just {adverb2} {verb2} away. \n"
+    f"As they were {verb2}ing away, they ran into another {S_noun}! {P_noun} grabbed a nearby {adjective2} {S_noun2} and started {adverb3} hitting the {S_noun}. Now, the {S_noun} is no longer a problem.")
